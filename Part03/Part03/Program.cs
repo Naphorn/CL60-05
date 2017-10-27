da044cc9f63f68df4c9901274af71b67830b52fa
Naphorn <naphorn.chok@gmail.com>
2017-10-27 20:58:13 +0700

    Use throw

---
+++ Part03/Part03/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+public class ExceptionLearning
+{
+    public static void Main()
+    {
+        int a = 10;
+        int b = 20;
+        int c = add(a, b);
+    }
+    private static int add(int a, int b)
+    {
+        throw new NotImplementedException();
+    }
+}
