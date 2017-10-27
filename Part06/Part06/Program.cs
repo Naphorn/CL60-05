cc1e0259c6f61d13dcb56c5c8488f5e7cc98ca29
Naphorn <naphorn.chok@gmail.com>
2017-10-27 21:59:09 +0700

    use for

---
+++ Part06/Part06/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class ForLoopTest
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
