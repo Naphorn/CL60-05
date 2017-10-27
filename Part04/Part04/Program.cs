901641c883632f746372ca7481fec0a17a890688
Naphorn <naphorn.chok@gmail.com>
2017-10-27 21:30:36 +0700

    use If

---
+++ Part04/Part04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+public class IfLearning
+{
+    public static void Main()
+    {
+        int a = 2;
+        if (a == 2)
+        {
+            Console.WriteLine("execute this line");
+        }
+        else
+        {
+            Console.WriteLine("execute another line too");
+        }
+        Console.WriteLine("this line is always execute");
+    }
+}
